a24adcd3e14e4589c1a7c763dfa21a84a460e441
Yamin <myamin335@gmail.com>
2022-05-31 17:12:49 +0600

    1. Created necessary data models from json file 2. Parsed json into data model 3. Created Base Service Module list 4. Data bound to Base Service Module list

--- app/src/main/java/net/store/divineit/MainActivity.kt
+++ app/src/main/java/net/store/divineit/MainActivity.kt
@@ -5,20 +5,46 @@ import android.os.Bundle
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.gson.Gson
 import net.store.divineit.databinding.ActivityMainBinding
+import net.store.divineit.models.BaseServiceModule
 import net.store.divineit.models.FinancialService
+import net.store.divineit.ui.BaseServiceModuleListAdapter
 import net.store.divineit.ui.FinancialServiceListAdapter
+import java.io.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: FinancialServiceListAdapter
     private lateinit var summarySheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var baseServiceAdapter: BaseServiceModuleListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val inputStream: InputStream = resources.openRawResource(R.raw.divine)
+        val writer: Writer = StringWriter()
+        val buffer = CharArray(1024)
+        inputStream.use { stream ->
+            val reader: Reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
+            var n: Int
+            while (reader.read(buffer).also { n = it } != -1) {
+                writer.write(buffer, 0, n)
+            }
+        }
+
+        val jsonString: String = writer.toString()
+        val baseModuleList = Gson().fromJson(jsonString, Array<BaseServiceModule>::class.java).toList()
+
+        baseServiceAdapter = BaseServiceModuleListAdapter {
+
+        }
+        baseServiceAdapter.submitList(baseModuleList)
+        binding.baseServiceModuleRecycler.adapter = baseServiceAdapter
 
         summarySheetBehavior = BottomSheetBehavior.from(binding.summarySheet.root)
 
---
+++ app/src/main/java/net/store/divineit/ui/BaseServiceModuleListAdapter.kt
@@ -0,0 +1,59 @@
+package net.store.divineit.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import net.store.divineit.R
+import net.store.divineit.databinding.BaseServiceModuleListItemBinding
+import net.store.divineit.models.BaseServiceModule
+
+class BaseServiceModuleListAdapter internal constructor(
+    private val callback: (BaseServiceModule) -> Unit
+) : RecyclerView.Adapter<BaseServiceModuleListAdapter.ViewHolder>() {
+
+    private var dataList: List<BaseServiceModule> = ArrayList()
+    private var selectedItemPosition = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: BaseServiceModuleListItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.list_item_base_service_module, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    fun submitList(dataList: List<BaseServiceModule>) {
+        this.dataList = dataList
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val binding: BaseServiceModuleListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val context = binding.root.context
+            val item = dataList[position]
+            binding.item = item
+            binding.root.setOnClickListener {
+                val previousPosition = selectedItemPosition
+                selectedItemPosition = position
+                callback(item)
+                notifyItemChanged(previousPosition)
+                notifyItemChanged(selectedItemPosition)
+            }
+
+            if (position == selectedItemPosition) {
+                binding.container.background = ContextCompat.getDrawable(context, R.drawable.rounded_rectangle_blue_3)
+                binding.name.setTextColor(ContextCompat.getColor(context, R.color.black))
+            } else {
+                binding.container.background = ContextCompat.getDrawable(context, R.drawable.rounded_rectangle_gray_2)
+                binding.name.setTextColor(ContextCompat.getColor(context, R.color.blue3))
+            }
+        }
+    }
+}
